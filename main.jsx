d49c896b0a3a68c79fbffde3bfaf404ec862bd82
Shankar2771 <167313958+Shankar2771@users.noreply.github.com>
2024-11-29 17:56:52 +0530

    Delete main.jsx

--- main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react' // imr
-import ReactDOM, { createRoot } from 'react-dom/client' // imrd
-import App from './App'
-import Task1 from './task/Task1'
-import "./global.css"
-
-createRoot(document.getElementById('root')).render(<App/>)
